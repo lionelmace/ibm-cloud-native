e0f1dd8b5c9db1fccecfcf45b6562a2cc14b6a0a
Lionel Mace <lionel.mace@gmail.com>
2023-09-11 09:23:49 +0200

    removed transit gateway to optimize cost

--- terraform/transit-gateway.tf
+++ terraform/transit-gateway.tf
@@ -1,14 +1,20 @@
+# Warning
+# The first two connections are free at the account level not the gateway level. 
+# So if you create 2 TGWs each with 2 connections you'd be charged for 2 connections, 
+# and if you create a TGW with 2 connections, delete it, 
+# and create again in the same month, you'd be charged for 2 connections.
+# TGW exceeded 2 connections it'd get charged for those extra connections.
 
-resource "ibm_tg_gateway" "tgw" {
-  name           = format("%s-%s", local.basename, "tgw")
-  location       = "eu-de"
-  global         = false
-  resource_group = ibm_resource_group.group.id
-}
+# resource "ibm_tg_gateway" "tgw" {
+#   name           = format("%s-%s", local.basename, "tgw")
+#   location       = "eu-de"
+#   global         = false
+#   resource_group = ibm_resource_group.group.id
+# }
 
-resource "ibm_tg_connection" "tgw_connection" {
-  gateway      = ibm_tg_gateway.tgw.id
-  network_type = "vpc"
-  name         = "my-tgw-connection"
-  network_id   = ibm_is_vpc.vpc.crn
-}
+# resource "ibm_tg_connection" "tgw_connection" {
+#   gateway      = ibm_tg_gateway.tgw.id
+#   network_type = "vpc"
+#   name         = "my-tgw-connection"
+#   network_id   = ibm_is_vpc.vpc.crn
+# }
