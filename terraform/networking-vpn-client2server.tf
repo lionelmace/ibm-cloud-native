c9c66ffc0217954370f223e2c0d830acda387618
Lionel Mace <lionel.mace@gmail.com>
2023-10-16 16:21:32 +0200

    cleanup tf vpn

--- terraform/networking-vpn-client2server.tf
+++ terraform/networking-vpn-client2server.tf
@@ -4,11 +4,9 @@ variable "vpn_client_ip_pool" {
 }
 
 resource "ibm_is_vpn_server" "vpn" {
-  # LMA certificate_crn = data.terraform_remote_state.certificates.outputs.server_cert_crn
   certificate_crn = ibm_sm_imported_certificate.server_cert.crn
   client_authentication {
     method        = "certificate"
-    # LMA client_ca_crn = data.terraform_remote_state.certificates.outputs.client_cert_crn
     client_ca_crn = ibm_sm_imported_certificate.client_cert.crn
   }
   client_ip_pool         = var.vpn_client_ip_pool
@@ -94,11 +92,9 @@ resource "ibm_is_vpn_server_route" "route_private_to_vpc" {
 
 data "ibm_is_vpn_server_client_configuration" "config" {
   vpn_server = ibm_is_vpn_server.vpn.id
-  # file_path  = "../config/client.ovpn"
 }
 
 output "full-ovpn-config" {
-  # sensitive = true
   value = <<EOT
 ${data.ibm_is_vpn_server_client_configuration.config.vpn_server_client_configuration}
 
@@ -111,21 +107,6 @@ ${nonsensitive(module.pki.certificates["client"].private_key.private_key_pem)}
 </key>
 EOT
 }
-
-# resource "local_file" "fullconfig" {
-#   content  = <<EOT
-# ${data.ibm_is_vpn_server_client_configuration.config.vpn_server_client_configuration}
-
-# <cert>
-# ${output.client_cert}
-# </cert>
-
-# <key>
-# ${output.client_key}
-# </key>
-# EOT
-#   filename = "../config/client-full.ovpn"
-# }
 
 output "vpn" {
   value = ibm_is_vpn_server.vpn
