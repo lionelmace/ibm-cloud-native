d8b96c46762b918c2a6712ca6a912820130a0d3b
Lionel Mace <lionel.mace@gmail.com>
2023-10-16 12:59:35 +0200

    clean up and local file

--- terraform/networking-vpn-client2server.tf
+++ terraform/networking-vpn-client2server.tf
@@ -1,10 +1,3 @@
-# data "terraform_remote_state" "certificates" {
-#   backend = "local"
-
-#   config = {
-#     path = "../010-certificates/terraform.tfstate"
-#   }
-# }
 
 variable "vpn_client_ip_pool" {
   default = "192.168.0.0/16"
@@ -100,20 +93,20 @@ data "ibm_is_vpn_server_client_configuration" "config" {
   file_path  = "../config/client.ovpn"
 }
 
-# resource "local_file" "fullconfig" {
-#   content  = <<EOT
-# ${data.ibm_is_vpn_server_client_configuration.config.vpn_server_client_configuration}
+resource "local_file" "fullconfig" {
+  content  = <<EOT
+${data.ibm_is_vpn_server_client_configuration.config.vpn_server_client_configuration}
 
-# <cert>
-# ${data.terraform_remote_state.certificates.outputs.client_cert}
-# </cert>
+<cert>
+${data.terraform_remote_state.certificates.outputs.client_cert}
+</cert>
 
-# <key>
-# ${data.terraform_remote_state.certificates.outputs.client_key}
-# </key>
-# EOT
-#   filename = "../config/client-full.ovpn"
-# }
+<key>
+${data.terraform_remote_state.certificates.outputs.client_key}
+</key>
+EOT
+  filename = "../config/client-full.ovpn"
+}
 
 output "vpn" {
   value = ibm_is_vpn_server.vpn
