0bf8f8c789f9051908814a61f31984c5c5d7f2bd
Lionel Mace <lionel.mace@gmail.com>
2023-10-16 12:01:22 +0200

    fix certif

--- terraform/networking-vpn-client2server.tf
+++ terraform/networking-vpn-client2server.tf
@@ -12,11 +12,11 @@ variable "vpn_client_ip_pool" {
 
 resource "ibm_is_vpn_server" "vpn" {
   # LMA certificate_crn = data.terraform_remote_state.certificates.outputs.server_cert_crn
-  certificate_crn = outputs.server_cert_crn
+  certificate_crn = ibm_sm_imported_certificate.server_cert.crn
   client_authentication {
     method        = "certificate"
     # LMA client_ca_crn = data.terraform_remote_state.certificates.outputs.client_cert_crn
-    client_ca_crn = outputs.client_cert_crn
+    client_ca_crn = ibm_sm_imported_certificate.client_cert.crn
   }
   client_ip_pool         = var.vpn_client_ip_pool
   client_idle_timeout    = 2800
