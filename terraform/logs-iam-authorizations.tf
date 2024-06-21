37fc8a5da191cad89d2dd16af17770fc67ba3786
Lionel Mace <lionel.mace@gmail.com>
2024-06-21 15:11:29 +0200

    added cloud logs

---
+++ terraform/logs-iam-authorizations.tf
@@ -0,0 +1,28 @@
+# IAM S2S Service to Service Authorization
+##############################################################################
+
+# S2S Authorization from Cloud Logs to COS
+resource "ibm_iam_authorization_policy" "cloud-logs-cos" {
+  source_service_name         = "logs"
+  source_resource_instance_id = ibm_resource_instance.logs_instance.guid
+  target_service_name         = "cloud-object-storage"
+  target_resource_instance_id = ibm_resource_instance.cos.guid
+  roles                       = ["Writer"]
+}
+
+# S2S Authorization from Logs Router to Cloud Logs
+# ALREADY EXIST
+# resource "ibm_iam_authorization_policy" "cloud-logs-router" {
+#   source_service_name = "logs-router"
+#   target_service_name = "logs"
+#   roles = ["Sender"]
+# }
+
+##############################################################################
+resource "ibm_iam_authorization_policy" "iam-auth-kms-cos" {
+  source_service_name         = "cloud-object-storage"
+  source_resource_instance_id = ibm_resource_instance.cos.guid
+  target_service_name         = "kms"
+  target_resource_instance_id = ibm_resource_instance.key-protect.guid
+  roles                       = ["Reader"]
+}
---
+++ terraform/logs-observability-instance.tf
@@ -0,0 +1,25 @@
+
+##############################################################################
+# Cloud Logs Services
+##############################################################################
+
+
+# Cloud Logs Resource
+##############################################################################
+
+resource "ibm_resource_instance" "logs_instance" {
+  resource_group_id = local.resource_group_id
+  name              = format("%s-%s", local.basename, "cloud-logs")
+  service           = "logs"
+  plan              = "beta"
+  location          = var.region
+  tags              = var.tags
+  service_endpoints = "private"
+  parameters = {
+    logs_bucket_crn = ibm_cos_bucket.bucket-data.crn
+    logs_bucket_endpoint = ibm_cos_bucket.bucket-data.s3_endpoint_direct
+    metrics_bucket_crn = ibm_cos_bucket.bucket-metrics.crn
+    metrics_bucket_endpoint = ibm_cos_bucket.bucket-metrics.s3_endpoint_direct
+    retention_period = 7
+  }
+}
