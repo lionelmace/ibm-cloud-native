6aa7ae6b8748dc386fae52b29c23c4433aaf4088
Lionel Mace <lionel.mace@gmail.com>
2024-09-06 17:17:14 +0200

    removed target

--- terraform/observability-at-routing.tf
+++ terraform/observability-at-routing.tf
@@ -1,20 +1,21 @@
-resource "ibm_atracker_target" "atracker_cloudlogs_target" {
-  cloudlogs_endpoint {
-    target_crn = ibm_resource_instance.logs_instance.id
-  }
-  name = "my-cloudlogs-target"
-  target_type = "cloud_logs"
-  region = "eu-de"
-}
+# Failed
+# resource "ibm_atracker_target" "atracker_cloudlogs_target" {
+#   cloudlogs_endpoint {
+#     target_crn = ibm_resource_instance.logs_instance.id
+#   }
+#   name = "my-cloudlogs-target"
+#   target_type = "cloud_logs"
+#   region = "eu-de"
+# }
 
-resource "ibm_atracker_route" "atracker_route" {
-  name = "my-route"
-  rules {
-    target_ids = [ ibm_atracker_target.atracker_cloudlogs_target.id ]
-    locations = [ "eu-de", "global" ]
-  }
-  lifecycle {
-    # Recommended to ensure that if a target ID is removed here and destroyed in a plan, this is updated first
-    create_before_destroy = true
-  }
-}
+# resource "ibm_atracker_route" "atracker_route" {
+#   name = "my-route"
+#   rules {
+#     target_ids = [ ibm_atracker_target.atracker_cloudlogs_target.id ]
+#     locations = [ "eu-de", "global" ]
+#   }
+#   lifecycle {
+#     # Recommended to ensure that if a target ID is removed here and destroyed in a plan, this is updated first
+#     create_before_destroy = true
+#   }
+# }
