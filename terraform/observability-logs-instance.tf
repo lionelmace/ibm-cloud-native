81982185d986ac1cacc717051c9f9d7d2594c750
Lionel Mace <lionel.mace@gmail.com>
2024-09-11 16:17:19 +0200

    removed duplicated at route

--- terraform/observability-logs-instance.tf
+++ terraform/observability-logs-instance.tf
@@ -27,8 +27,6 @@ resource "ibm_resource_instance" "logs_instance" {
 }
 
 # Cloud Logs Routing
-#
-# Warning: Conflicting if deployed in shared account that already has a target
 ##############################################################################
 resource "ibm_logs_router_tenant" "logs_router_tenant_instance" {
   name = format("%s-%s", local.basename, "cloud-logs-router")
@@ -51,28 +49,4 @@ resource "ibm_logs_router_tenant" "logs_router_tenant_instance" {
       access_credential = module.log_analysis.ingestion_key
     }
   }
-}
-
-# Activity Tracker Event Routing
-##############################################################################
-resource "ibm_atracker_route" "atracker_route" {
-  name = format("%s-%s", local.basename, "at-route")
-  rules {
-    target_ids = [ibm_atracker_target.atracker_cloudlogs_target.id]
-    locations  = [var.region, "global"]
-  }
-  lifecycle {
-    # Recommended to ensure that if a target ID is removed here and destroyed in a plan, this is updated first
-    create_before_destroy = true
-  }
-  depends_on = [ibm_iam_authorization_policy.iam-auth-atracker-2-logs]
-}
-
-resource "ibm_atracker_target" "atracker_cloudlogs_target" {
-  cloudlogs_endpoint {
-    target_crn = ibm_resource_instance.logs_instance.id
-  }
-  name        = format("%s-%s", local.basename, "cloudlogs-target")
-  target_type = "cloud_logs"
-  region      = var.region
 }
