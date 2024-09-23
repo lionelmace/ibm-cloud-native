2863ae7597ce909302bb732654c2f72479183c3e
Lionel Mace <lionel.mace@gmail.com>
2024-09-23 17:47:39 +0200

    added cloud logs agent for roks cluster via terraform module

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -27,4 +27,19 @@ provider "ibm" {
   region           = var.region
 }
 
-##############################################################################
+provider "helm" {
+  kubernetes {
+    host                   = data.ibm_container_cluster_config.cluster_config.host
+    token                  = data.ibm_container_cluster_config.cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
+  }
+}
+
+# ############################################################################
+# Init cluster config for helm
+# ############################################################################
+
+data "ibm_container_cluster_config" "roks_cluster_config" {
+  # update this value with the Id of the cluster where these agents will be provisioned
+  cluster_name_id = ibm_container_vpc_cluster.roks_cluster.id
+}
