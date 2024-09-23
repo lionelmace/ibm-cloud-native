baa8ca7e52fc843e7d175d88b1882827d29873a8
Lionel Mace <lionel.mace@gmail.com>
2024-09-23 17:54:46 +0200

    fix roks cluster config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -29,9 +29,9 @@ provider "ibm" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.ibm_container_cluster_config.cluster_config.host
-    token                  = data.ibm_container_cluster_config.cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
+    host                   = data.ibm_container_cluster_config.roks_cluster_config.host
+    token                  = data.ibm_container_cluster_config.roks_cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
   }
 }
 
