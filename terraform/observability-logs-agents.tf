622ea04d4db7d21f5346fd74035fc7cbb772b894
Lionel Mace <lionel.mace@gmail.com>
2024-09-23 17:53:01 +0200

    added logs agent

--- terraform/observability-logs-agents.tf
+++ terraform/observability-logs-agents.tf
@@ -2,10 +2,10 @@
 # Install observability agents
 # ############################################################################
 
-# module "observability_agents" {
-#   source                           = "terraform-ibm-modules/observability-agents/ibm"
-#   version                          = "1.2.4"
-#   is_vpc_cluster                   = true
-#   cluster_id                       = ibm_container_vpc_cluster.roks_cluster.id
-#   cluster_resource_group_id        = ibm_container_vpc_cluster.roks_cluster.id
-# }
+module "observability_agents" {
+  source                           = "terraform-ibm-modules/observability-agents/ibm"
+  version                          = "1.2.4"
+  is_vpc_cluster                   = true
+  cluster_id                       = ibm_container_vpc_cluster.roks_cluster.id
+  cluster_resource_group_id        = ibm_container_vpc_cluster.roks_cluster.id
+}
