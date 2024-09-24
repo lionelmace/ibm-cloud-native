4056b2b9d2a5e6222b5a3832685a879d2e873df3
Lionel Mace <lionel.mace@gmail.com>
2024-09-24 11:55:03 +0200

    disabled cloud_monitoring_enabled

--- terraform/observability-logs-agents.tf
+++ terraform/observability-logs-agents.tf
@@ -3,9 +3,10 @@
 # ############################################################################
 
 module "observability_agents" {
-  source                           = "terraform-ibm-modules/observability-agents/ibm"
-  is_vpc_cluster                   = true
-  cluster_id                       = ibm_container_vpc_cluster.roks_cluster.id
-  cluster_resource_group_id        = ibm_container_vpc_cluster.roks_cluster.id
-  log_analysis_enabled = false
+  source                    = "terraform-ibm-modules/observability-agents/ibm"
+  is_vpc_cluster            = true
+  cluster_id                = ibm_container_vpc_cluster.roks_cluster.id
+  cluster_resource_group_id = ibm_container_vpc_cluster.roks_cluster.id
+  log_analysis_enabled      = false
+  cloud_monitoring_enabled  = false
 }
