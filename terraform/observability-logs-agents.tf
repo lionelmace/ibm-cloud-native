6c80f892c9a9577bae8f2485a39e50b772ccb5a9
Lionel Mace <lionel.mace@gmail.com>
2024-09-24 12:20:50 +0200

    removed logs observability agents

--- terraform/observability-logs-agents.tf
+++ terraform/observability-logs-agents.tf
@@ -2,11 +2,12 @@
 # Install observability agents
 # ############################################################################
 
-module "observability_agents" {
-  source                    = "terraform-ibm-modules/observability-agents/ibm"
-  is_vpc_cluster            = true
-  cluster_id                = ibm_container_vpc_cluster.roks_cluster.id
-  cluster_resource_group_id = ibm_resource_group.group.id
-  log_analysis_enabled      = false
-  cloud_monitoring_enabled  = false
-}
+# Cloud Logs Not supported yet.
+# module "observability_agents" {
+#   source                    = "terraform-ibm-modules/observability-agents/ibm"
+#   is_vpc_cluster            = true
+#   cluster_id                = ibm_container_vpc_cluster.roks_cluster.id
+#   cluster_resource_group_id = ibm_resource_group.group.id
+#   log_analysis_enabled      = false
+#   cloud_monitoring_enabled  = false
+# }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -27,20 +27,21 @@ provider "ibm" {
   region           = var.region
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.ibm_container_cluster_config.roks_cluster_config.host
-    token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
-  }
-}
+# Cloud Logs Not supported yet.
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
+#     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
+#     cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+#   }
+# }
 
 # ############################################################################
 # Init cluster config for helm
 # ############################################################################
 
-data "ibm_container_cluster_config" "roks_cluster_config" {
-  # update this value with the cluster ID where these agents will be provisioned
-  cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
-  resource_group_id = ibm_resource_group.group.id
-}
+# data "ibm_container_cluster_config" "roks_cluster_config" {
+#   # update this value with the cluster ID where these agents will be provisioned
+#   cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
+#   resource_group_id = ibm_resource_group.group.id
+# }
