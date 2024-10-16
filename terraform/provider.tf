042c3e95b2a85fadb1549290902375b81e76d879
Lionel Mace <lionel.mace@gmail.com>
2024-10-16 12:42:49 +0200

    fix authenticate to the helm registry in the provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -27,21 +27,24 @@ provider "ibm" {
   region           = var.region
 }
 
-# Cloud Logs Not supported yet.
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
-#     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-#     cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = data.ibm_container_cluster_config.roks_cluster_config.host
+    token                  = data.ibm_container_cluster_config.roks_cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+  }
+  # IBM Cloud credentials are required to authenticate to the helm repo
+  registry {
+    url = "oci://icr.io/ibm/observe/logs-agent-helm"
+    username = "iamapikey"
+    password = var.ibmcloud_api_key # replace with an IBM cloud apikey
+  }
+}
 
-# ############################################################################
 # Init cluster config for helm
 # ############################################################################
-
-# data "ibm_container_cluster_config" "roks_cluster_config" {
-#   # update this value with the cluster ID where these agents will be provisioned
-#   cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
-#   resource_group_id = ibm_resource_group.group.id
-# }
+data "ibm_container_cluster_config" "roks_cluster_config" {
+  # update this value with the cluster ID where these agents will be provisioned
+  cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
+  resource_group_id = ibm_resource_group.group.id
+}
