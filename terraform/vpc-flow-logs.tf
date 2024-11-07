5c21df9c329f2f8040ba6382f36b4ca76380b589
Lionel Mace <lionel.mace@gmail.com>
2024-11-07 15:54:40 +0100

    fix vpc flow logs cos and target

--- terraform/vpc-flow-logs.tf
+++ terraform/vpc-flow-logs.tf
@@ -12,7 +12,7 @@ resource "ibm_resource_instance" "cos-flow-logs" {
 
 resource "ibm_cos_bucket" "bucket-flow-logs" {
   bucket_name          = format("%s-%s", local.basename, "bucket-flow-logs")
-  resource_instance_id = ibm_resource_instance.example.id
+  resource_instance_id = ibm_resource_instance.cos-flow-logs.id
   region_location      = var.region
   storage_class        = "standard"
 }
@@ -20,7 +20,7 @@ resource "ibm_cos_bucket" "bucket-flow-logs" {
 resource "ibm_is_flow_log" "example" {
   depends_on     = [ibm_cos_bucket.bucket-flow-logs]
   name           = format("%s-%s", local.basename, "flow-logs")
-#   target         = ibm_is_instance.example.id
+  target         = ibm_is_vpc.vpc.id
   active         = true
   storage_bucket = ibm_cos_bucket.bucket-flow-logs.bucket_name
 }
