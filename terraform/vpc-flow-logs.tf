edc9c69dba7751b54a3c35f12e81ae452167facf
Lionel Mace <lionel.mace@gmail.com>
2024-11-07 15:52:47 +0100

    added vpc flow logs

---
+++ terraform/vpc-flow-logs.tf
@@ -0,0 +1,26 @@
+
+## VPC Flow Logs
+##############################################################################
+
+resource "ibm_resource_instance" "cos-flow-logs" {
+  name              = format("%s-%s", local.basename, "cos-flow-logs")
+  resource_group_id = ibm_resource_group.group.id
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+}
+
+resource "ibm_cos_bucket" "bucket-flow-logs" {
+  bucket_name          = format("%s-%s", local.basename, "bucket-flow-logs")
+  resource_instance_id = ibm_resource_instance.example.id
+  region_location      = var.region
+  storage_class        = "standard"
+}
+
+resource "ibm_is_flow_log" "example" {
+  depends_on     = [ibm_cos_bucket.bucket-flow-logs]
+  name           = format("%s-%s", local.basename, "flow-logs")
+#   target         = ibm_is_instance.example.id
+  active         = true
+  storage_bucket = ibm_cos_bucket.bucket-flow-logs.bucket_name
+}
