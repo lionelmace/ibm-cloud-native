77eafecab37977afe32f723d727326c058fdf541
Lionel Mace <lionel.mace@gmail.com>
2024-12-02 09:44:19 +0100

    removed duplicated variable for cos bucket

--- terraform/storage-cos-backup-pvc.tf
+++ terraform/storage-cos-backup-pvc.tf
@@ -2,28 +2,14 @@
 # COS Instance with 1 bucket
 ##############################################################################
 
-# COS Variables
-##############################################################################
-variable "cos_plan" {
-  description = "COS plan type"
-  type        = string
-  default     = "standard"
-}
-
-variable "cos_region" {
-  description = "Enter Region for provisioning"
-  type        = string
-  default     = "global"
-}
-
 # COS Service for OpenShift Internal Registry
 ##############################################################################
 
 resource "ibm_resource_instance" "cos-backup" {
   name              = format("%s-%s", local.basename, "cos-backup")
   service           = "cloud-object-storage"
-  plan              = var.cos_plan
-  location          = var.cos_region
+  plan              = "standard"
+  location          = "global"
   resource_group_id = ibm_resource_group.group.id
   tags              = var.tags
 
