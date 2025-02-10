137dd515d24ae50fd850720e718e3b807e894c8a
Lionel Mace <lionel.mace@gmail.com>
2025-02-10 11:47:55 +0100

    added iam Authorization policy between Cloud Monitoring (Source) and Event Notifications (Target)

--- terraform/devops-notifications.tf
+++ terraform/devops-notifications.tf
@@ -5,3 +5,13 @@ resource "ibm_resource_instance" "event-notifications" {
   location          = var.region
   resource_group_id = ibm_resource_group.group.id
 }
+
+# Authorization policy between Cloud Monitoring (Source) and Event Notifications (Target)
+# Required to add a Notification Channel with Event Notification in Cloud Monitoring
+resource "ibm_iam_authorization_policy" "iam-auth-kms-cos" {
+  source_service_name         = "sysdig-monitor"
+  source_resource_instance_id = module.cloud_monitoring.guid
+  target_service_name         = "event-notifications"
+  target_resource_instance_id = ibm_resource_instance.event-notifications.guid
+  roles                       = ["Reader", "Event Source Manager"]
+}
