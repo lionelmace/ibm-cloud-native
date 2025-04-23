84fd28bdeb839424554c6e09bba740ccbc9610be
Lionel Mace <lionel.mace@gmail.com>
2025-04-23 09:48:03 +0200

    updated provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,15 +26,23 @@ provider "ibm" {
 provider "kubernetes" {
   host                   = data.ibm_container_cluster_config.roks_cluster_config.host
   token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-  cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+  # cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+    # cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
   }
+}
+
+# Init cluster config for helm and kubernetes providers
+############################################################################
+data "ibm_container_cluster_config" "roks_cluster_config" {
+  cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
+  resource_group_id = ibm_resource_group.group.id
+  endpoint_type     = "private"
 }
 
 # Helm to install IBM Cloud Logs
@@ -54,11 +62,3 @@ provider "helm" {
 #     password = var.ibmcloud_api_key
 #   }
 # }
-
-# Init cluster config for helm
-############################################################################
-data "ibm_container_cluster_config" "roks_cluster_config" {
-  # update this value with the cluster ID where these agents will be provisioned
-  cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
-  resource_group_id = ibm_resource_group.group.id
-}
