1030a34237de3c04cb33ef5856af9aa53d120850
Lionel Mace <lionel.mace@gmail.com>
2025-04-23 09:51:30 +0200

    uncommented cluster certificate

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,14 +26,14 @@ provider "ibm" {
 provider "kubernetes" {
   host                   = data.ibm_container_cluster_config.roks_cluster_config.host
   token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-  # cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+  cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-    # cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
   }
 }
 
