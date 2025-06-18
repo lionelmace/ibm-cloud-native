6def0a5a215accba994c584d6f2f5c5ee80b3c1a
Lionel Mace <lionel.mace@gmail.com>
2025-06-18 17:23:32 +0200

    fix load_config_file = false

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -40,8 +40,8 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  load_config_file = false
   kubernetes {
+    load_config_file = false
     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
     cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
