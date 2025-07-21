45e8425d31f4e515fa3e5e5c3bdc486cfe121dd7
Lionel Mace <lionel.mace@gmail.com>
2025-07-21 13:17:34 +0200

    rollback SCC is deprecated

--- terraform/security-scc-cos.tf
+++ terraform/security-scc-cos.tf
@@ -34,35 +34,36 @@ resource "ibm_resource_instance" "cos-scc" {
 }
 
 ## COS Bucket
+## SCC is DEPRECATED
 ##############################################################################
-resource "ibm_cos_bucket" "scc-bucket" {
-  bucket_name          = format("%s-%s-%s", local.basename, "scc-bucket", random_string.random.result)
-  resource_instance_id = ibm_resource_instance.cos-scc.id
-  storage_class        = "smart"
+# resource "ibm_cos_bucket" "scc-bucket" {
+#   bucket_name          = format("%s-%s-%s", local.basename, "scc-bucket", random_string.random.result)
+#   resource_instance_id = ibm_resource_instance.cos-scc.id
+#   storage_class        = "smart"
 
-  # SCC Control 2.1.1.2
-  # Ensure Cloud Object Storage encryption is enabled with BYOK
-  # Key management services can only be added during bucket creation.
-  depends_on  = [ibm_iam_authorization_policy.iam-auth-kms-cos]
-  kms_key_crn = ibm_kms_key.key.id
+#   # SCC Control 2.1.1.2
+#   # Ensure Cloud Object Storage encryption is enabled with BYOK
+#   # Key management services can only be added during bucket creation.
+#   depends_on  = [ibm_iam_authorization_policy.iam-auth-kms-cos]
+#   kms_key_crn = ibm_kms_key.key.id
 
-  # SCC requires Cross-Region bucket for resiliency
-  cross_region_location = "eu"
-  # region_location      = "eu-de"
+#   # SCC requires Cross-Region bucket for resiliency
+#   cross_region_location = "eu"
+#   # region_location      = "eu-de"
 
-  # Activity Tracked is Deprecated
-  # activity_tracking {
-  #   read_data_events     = true
-  #   write_data_events    = true
-  #   activity_tracker_crn = local.activity_tracker_id
-  # }
-  metrics_monitoring {
-    usage_metrics_enabled   = true
-    request_metrics_enabled = true
-    metrics_monitoring_crn  = module.cloud_monitoring.crn
-  }
-  endpoint_type = "public"
-}
+#   # Activity Tracked is Deprecated
+#   # activity_tracking {
+#   #   read_data_events     = true
+#   #   write_data_events    = true
+#   #   activity_tracker_crn = local.activity_tracker_id
+#   # }
+#   metrics_monitoring {
+#     usage_metrics_enabled   = true
+#     request_metrics_enabled = true
+#     metrics_monitoring_crn  = module.cloud_monitoring.crn
+#   }
+#   endpoint_type = "public"
+# }
 
 
 ## IAM
