5d8b2479fd02e8471c428445f005af3e5e797b22
Lionel Mace <lionel.mace@gmail.com>
2025-09-01 09:56:59 +0200

    upgraded tf provider to 1.12

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 terraform {
-  required_version = ">=1.10"
+  required_version = ">=1.12"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
