d812831434f722c652da7bf385f40cbb53e9ce61
Lionel Mace <lionel.mace@gmail.com>
2025-11-13 15:30:12 +0100

    added ibm_dns_glb_monitor

--- terraform/networking-private-dns.tf
+++ terraform/networking-private-dns.tf
@@ -39,8 +39,32 @@ resource "ibm_dns_glb_pool" "pool-nlb-1" {
     enabled     = true
     description = "origin pool"
   }
-  # monitor              = ibm_dns_glb_monitor.pdns-glb-monitor.monitor_id
-  # notification_channel = "https://mywebsite.com/dns/webhook"
-  # healthcheck_region   = "us-south"
-  # healthcheck_subnets  = [ibm_is_subnet.subnet[1].resource_crn]
+  monitor              = ibm_dns_glb_monitor.pdns-zone-1-monitor.monitor_id
+  notification_channel = "https://mywebsite.com/dns/webhook"
+  healthcheck_region   = var.region
+  # Option 1 – single subnet:
+  # healthcheck_subnets = [ibm_is_subnet.subnet[1].id]
+  # Option 2 – all three workload subnets:
+  # healthcheck_subnets = [
+  #   for s in ibm_is_subnet.subnet : s.crn
+  # ]
+}
+
+resource "ibm_dns_glb_monitor" "pdns-zone-1-monitor" {
+  depends_on     = [ibm_dns_zone.pdns-1-zone]
+  name           = "${local.basename}-pdns-glb-monitor"
+  instance_id    = ibm_resource_instance.pdns-instance.guid
+  interval       = 63
+  retries        = 3
+  timeout        = 8
+  port           = 8080
+  type           = "HTTP"
+  expected_codes = "200"
+  path           = "/health"
+  method         = "GET"
+  expected_body  = "alive"
+  headers {
+    name  = "headerName"
+    value = ["example", "abc"]
+  }
 }
