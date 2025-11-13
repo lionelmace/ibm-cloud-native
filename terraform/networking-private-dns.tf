3972ba61f242d356de7dfe24d1e4b5dc0b7721cd
Lionel Mace <lionel.mace@gmail.com>
2025-11-13 16:14:48 +0100

    fix zone in permitted networks of Private DNS

--- terraform/networking-private-dns.tf
+++ terraform/networking-private-dns.tf
@@ -12,23 +12,23 @@ resource "ibm_resource_instance" "pdns-instance" {
 # DNS Zone
 ##############################################################################
 
-resource "ibm_dns_zone" "pdns-1-zone" {
+resource "ibm_dns_zone" "pdns-zone-1" {
   name = "${local.basename}.local"
   instance_id = ibm_resource_instance.pdns-instance.guid
 }
 
 resource "ibm_dns_resource_record" "pdns-a-record" {
   instance_id = ibm_resource_instance.pdns-instance.guid
-  zone_id     = ibm_dns_zone.pdns-1-zone.zone_id
+  zone_id     = ibm_dns_zone.pdns-zone-1.zone_id
   type        = "A"
   name        = "nlb-zone-1"
   rdata       = "10.243.1.8" # Private IP of my private NLB
-  ttl         = 3600
+  ttl         = 900
 }
 
 resource "ibm_dns_permitted_network" "pdns-permitted-network" {
     instance_id = ibm_resource_instance.pdns-instance.guid
-    zone_id = ibm_dns_zone.pdns-zone.zone_id
+    zone_id = ibm_dns_zone.pdns-zone-1.zone_id
     vpc_crn = ibm_is_vpc.vpc.id
     type = "vpc"
 }
@@ -40,7 +40,7 @@ resource "ibm_dns_glb" "pdns-glb" {
   depends_on    = [ibm_dns_glb_pool.pool-nlb-1]
   name          = "${local.basename}-nlb-glb"
   instance_id   = ibm_resource_instance.pdns-instance.guid
-  zone_id       = ibm_dns_zone.pdns-1-zone.zone_id
+  zone_id       = ibm_dns_zone.pdns-zone-1.zone_id
   ttl           = 120
   enabled       = true
   fallback_pool = ibm_dns_glb_pool.pool-nlb-1.pool_id
@@ -52,7 +52,7 @@ resource "ibm_dns_glb" "pdns-glb" {
 }
 
 resource "ibm_dns_glb_pool" "pool-nlb-1" {
-  depends_on                = [ibm_dns_zone.pdns-1-zone]
+  depends_on                = [ibm_dns_zone.pdns-zone-1]
   name                      = "testpool"
   instance_id               = ibm_resource_instance.pdns-instance.guid
   enabled                   = true
@@ -75,7 +75,7 @@ resource "ibm_dns_glb_pool" "pool-nlb-1" {
 }
 
 resource "ibm_dns_glb_monitor" "pdns-zone-1-monitor" {
-  depends_on     = [ibm_dns_zone.pdns-1-zone]
+  depends_on     = [ibm_dns_zone.pdns-zone-1]
   name           = "${local.basename}-pdns-glb-monitor"
   instance_id    = ibm_resource_instance.pdns-instance.guid
   interval       = 63
