084372c7c7d75f5a8cea3bd620931f66b73564c6
Lionel Mace <lionel.mace@gmail.com>
2025-11-14 09:19:26 +0100

    added glb pool

--- terraform/networking-private-dns.tf
+++ terraform/networking-private-dns.tf
@@ -12,23 +12,23 @@ resource "ibm_resource_instance" "pdns-instance" {
 # DNS Zone
 ##############################################################################
 
-resource "ibm_dns_zone" "pdns-zone-1" {
+resource "ibm_dns_zone" "pdns-zone" {
   name = "${local.basename}.local"
   instance_id = ibm_resource_instance.pdns-instance.guid
 }
 
-resource "ibm_dns_resource_record" "pdns-a-record" {
-  instance_id = ibm_resource_instance.pdns-instance.guid
-  zone_id     = ibm_dns_zone.pdns-zone-1.zone_id
-  type        = "A"
-  name        = "nlb-zone-1"
-  rdata       = "10.243.1.8" # Private IP of my private NLB
-  ttl         = 900
-}
+# resource "ibm_dns_resource_record" "pdns-a-record" {
+#   instance_id = ibm_resource_instance.pdns-instance.guid
+#   zone_id     = ibm_dns_zone.pdns-zone.zone_id
+#   type        = "A"
+#   name        = "nlb-zone-1"
+#   rdata       = "10.243.1.8" # Private IP of my private NLB
+#   ttl         = 900
+# }
 
 resource "ibm_dns_permitted_network" "pdns-permitted-network" {
     instance_id = ibm_resource_instance.pdns-instance.guid
-    zone_id = ibm_dns_zone.pdns-zone-1.zone_id
+    zone_id = ibm_dns_zone.pdns-zone.zone_id
     vpc_crn = ibm_is_vpc.vpc.crn
     type = "vpc"
 }
@@ -40,7 +40,7 @@ resource "ibm_dns_glb" "pdns-glb" {
   depends_on    = [ibm_dns_glb_pool.pool-nlb-1]
   name          = "${local.basename}-nlb-glb"
   instance_id   = ibm_resource_instance.pdns-instance.guid
-  zone_id       = ibm_dns_zone.pdns-zone-1.zone_id
+  zone_id       = ibm_dns_zone.pdns-zone.zone_id
   ttl           = 120
   enabled       = true
   fallback_pool = ibm_dns_glb_pool.pool-nlb-1.pool_id
@@ -49,34 +49,22 @@ resource "ibm_dns_glb" "pdns-glb" {
     availability_zone = "${var.region}-1"
     pools             = [ibm_dns_glb_pool.pool-nlb-1.pool_id]
   }
-}
-
-resource "ibm_dns_glb_pool" "pool-nlb-1" {
-  depends_on                = [ibm_dns_zone.pdns-zone-1]
-  name                      = "testpool"
-  instance_id               = ibm_resource_instance.pdns-instance.guid
-  enabled                   = true
-  healthy_origins_threshold = 1
-  origins {
-    name        = "example-1"
-    address     = "www.google.com"
-    enabled     = true
-    description = "origin pool"
+  az_pools {
+    availability_zone = "${var.region}-2"
+    pools             = [ibm_dns_glb_pool.pool-nlb-2.pool_id]
   }
-  monitor              = ibm_dns_glb_monitor.pdns-zone-1-monitor.monitor_id
-  notification_channel = "https://mywebsite.com/dns/webhook"
-  healthcheck_region   = var.region
-  # Option 1 – single subnet:
-  healthcheck_subnets = [ibm_is_subnet.subnet[1].id]
-  # Option 2 – all three workload subnets:
-  # healthcheck_subnets = [
-  #   for s in ibm_is_subnet.subnet : s.crn
-  # ]
+  az_pools {
+    availability_zone = "${var.region}-3"
+    pools             = [ibm_dns_glb_pool.pool-nlb-3.pool_id]
+  }
 }
 
-resource "ibm_dns_glb_monitor" "pdns-zone-1-monitor" {
-  depends_on     = [ibm_dns_zone.pdns-zone-1]
-  name           = "${local.basename}-pdns-glb-monitor"
+# GLB Monitor
+##############################################################################
+
+resource "ibm_dns_glb_monitor" "pdns-zone-monitor" {
+  depends_on     = [ibm_dns_zone.pdns-zone]
+  name           = "${local.basename}-pdns-glb-monitor-1"
   instance_id    = ibm_resource_instance.pdns-instance.guid
   interval       = 63
   retries        = 3
@@ -91,4 +79,82 @@ resource "ibm_dns_glb_monitor" "pdns-zone-1-monitor" {
     name  = "headerName"
     value = ["example", "abc"]
   }
+}
+
+# GLB - Pool 1
+##############################################################################
+
+resource "ibm_dns_glb_pool" "pool-nlb-1" {
+  depends_on                = [ibm_dns_zone.pdns-zone]
+  name                      = "pool-nlb-1"
+  instance_id               = ibm_resource_instance.pdns-instance.guid
+  enabled                   = true
+  healthy_origins_threshold = 1
+  origins {
+    name        = "example-1"
+    address     = "www.google.com"
+    enabled     = true
+    description = "origin pool"
+  }
+  monitor              = ibm_dns_glb_monitor.pdns-zone-monitor.monitor_id
+  notification_channel = "https://mywebsite.com/dns/webhook"
+  healthcheck_region   = var.region
+  # Option 1 – single subnet:
+  healthcheck_subnets = [ibm_is_subnet.subnet[1].id]
+  # Option 2 – all three workload subnets:
+  # healthcheck_subnets = [
+  #   for s in ibm_is_subnet.subnet : s.crn
+  # ]
+}
+
+# GLB - Pool 2
+##############################################################################
+
+resource "ibm_dns_glb_pool" "pool-nlb-2" {
+  depends_on                = [ibm_dns_zone.pdns-zone]
+  name                      = "pool-nlb-2"
+  instance_id               = ibm_resource_instance.pdns-instance.guid
+  enabled                   = true
+  healthy_origins_threshold = 1
+  origins {
+    name        = "example-1"
+    address     = "www.google.com"
+    enabled     = true
+    description = "origin pool"
+  }
+  monitor              = ibm_dns_glb_monitor.pdns-zone-monitor.monitor_id
+  notification_channel = "https://mywebsite.com/dns/webhook"
+  healthcheck_region   = var.region
+  # Option 1 – single subnet:
+  healthcheck_subnets = [ibm_is_subnet.subnet[1].id]
+  # Option 2 – all three workload subnets:
+  # healthcheck_subnets = [
+  #   for s in ibm_is_subnet.subnet : s.crn
+  # ]
+}
+
+# GLB - Pool 3
+##############################################################################
+
+resource "ibm_dns_glb_pool" "pool-nlb-3" {
+  depends_on                = [ibm_dns_zone.pdns-zone]
+  name                      = "pool-nlb-3"
+  instance_id               = ibm_resource_instance.pdns-instance.guid
+  enabled                   = true
+  healthy_origins_threshold = 1
+  origins {
+    name        = "example-1"
+    address     = "www.google.com"
+    enabled     = true
+    description = "origin pool"
+  }
+  monitor              = ibm_dns_glb_monitor.pdns-zone-monitor.monitor_id
+  notification_channel = "https://mywebsite.com/dns/webhook"
+  healthcheck_region   = var.region
+  # Option 1 – single subnet:
+  healthcheck_subnets = [ibm_is_subnet.subnet[1].id]
+  # Option 2 – all three workload subnets:
+  # healthcheck_subnets = [
+  #   for s in ibm_is_subnet.subnet : s.crn
+  # ]
 }
