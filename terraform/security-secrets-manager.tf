f610049580cd8c6b6e6e9af9d5104e6616df7ee8
Lionel Mace <lionel.mace@gmail.com>
2026-04-30 14:50:10 +0200

    replace existing_secrets_manager_name by existing_secrets_manager_crn to support cross region reference

--- terraform/security-secrets-manager.tf
+++ terraform/security-secrets-manager.tf
@@ -2,14 +2,21 @@
 ## Create a Secrets Manager instance or reuse an existing one
 ##############################################################################
 
-variable "existing_secrets_manager_name" {
+#LMA variable "existing_secrets_manager_name" {
+#   description = "Only one Trial plan of Secrets Manager is allowed per account. If this account already has an instance, enter the Resource Name."
+#   type        = string
+#   default     = ""
+# }
+
+variable "existing_secrets_manager_crn" {
   description = "Only one Trial plan of Secrets Manager is allowed per account. If this account already has an instance, enter the CRN (Cloud Resource Name)."
   type        = string
   default     = ""
 }
 
 resource "ibm_resource_instance" "secrets_manager" {
-  count             = var.existing_secrets_manager_name != "" ? 0 : 1
+  #LMA count             = var.existing_secrets_manager_name != "" ? 0 : 1
+  count             = var.existing_secrets_manager_crn != "" ? 0 : 1
   name              = format("%s-%s", local.basename, "secrets-manager")
   service           = "secrets-manager"
   plan              = "trial"
@@ -27,13 +34,17 @@ resource "ibm_resource_instance" "secrets_manager" {
 # }
 
 data "ibm_resource_instance" "secrets_manager" {
-  count = var.existing_secrets_manager_name != "" ? 1 : 0
-  name  = var.existing_secrets_manager_name
+  #LMA count = var.existing_secrets_manager_name != "" ? 1 : 0
+  #LMA name  = var.existing_secrets_manager_name
+  count       = var.existing_secrets_manager_crn != "" ? 1 : 0
+  identifier  = var.existing_secrets_manager_crn
 }
 
 locals {
-  secrets_manager_id   = var.existing_secrets_manager_name != "" ? data.ibm_resource_instance.secrets_manager.0.id : ibm_resource_instance.secrets_manager.0.id
-  secrets_manager_guid = var.existing_secrets_manager_name != "" ? data.ibm_resource_instance.secrets_manager.0.guid : ibm_resource_instance.secrets_manager.0.guid
+  #LMA secrets_manager_id   = var.existing_secrets_manager_name != "" ? data.ibm_resource_instance.secrets_manager.0.id : ibm_resource_instance.secrets_manager.0.id
+  #LMA secrets_manager_guid = var.existing_secrets_manager_name != "" ? data.ibm_resource_instance.secrets_manager.0.guid : ibm_resource_instance.secrets_manager.0.guid
+  secrets_manager_id   = var.existing_secrets_manager_crn != "" ? data.ibm_resource_instance.secrets_manager.0.id : ibm_resource_instance.secrets_manager.0.id
+  secrets_manager_guid = var.existing_secrets_manager_crn != "" ? data.ibm_resource_instance.secrets_manager.0.guid : ibm_resource_instance.secrets_manager.0.guid
 }
 
 output "secrets_manager_id" {
