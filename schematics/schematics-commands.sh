ed72f471f855b695b6c533df6cfb7e81e853b428
Lionel Mace <lionel.mace@gmail.com>
2026-06-08 19:18:17 +0200

    minor script change

--- schematics/schematics-commands.sh
+++ schematics/schematics-commands.sh
@@ -15,5 +15,9 @@ ibmcloud sch workspace state rm --id $WORKSPACE_ID --address "ibm_iam_user_invit
 ibmcloud sch apply --id $WORKSPACE_ID --target ibm_iam_user_invite.invite_vmware_users
 
 
-ibmcloud sch workspace state rm $WORKSPACE_ID --address 'module.backup_recover_protect_ocp.module.backup_recovery_instance.ibm_backup_recovery_connection_registration_token.registration_token[0]'
+ibmcloud sch workspace state rm --id $WORKSPACE_ID --address 'module.backup_recover_protect_ocp.module.backup_recovery_instance.ibm_backup_recovery_connection_registration_token.registration_token[0]'
+
+ibmcloud sch workspace state rm --id $WORKSPACE_ID --address 'module.backup_recover_protect_ocp.kubernetes_namespace_v1.dsc_namespace'
+ibmcloud sch workspace state rm --id $WORKSPACE_ID --address 'module.backup_recover_protect_ocp.kubernetes_namespace_v1.dsc_namespace'
+ibmcloud sch workspace state rm --id $WORKSPACE_ID --address 'module.backup_recover_protect_ocp.kubernetes_service_account_v1.brsagent'
 ##############################################################################
